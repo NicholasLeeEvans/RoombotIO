f515155c1249fac3193cff0de9d93ffbba28404e
NicholasLeeEvans <evansnicholaslee@gmail.com>
2026-05-01 17:23:23 +0900

    Revert "Perf/ir sensor noise"

--- src/main.cpp
+++ src/main.cpp
@@ -55,11 +55,8 @@ void IRAM_ATTR Timer_ISR_RangeFinder()
 unsigned long last_position_update;
 #define POSITION_UPDATE_MS 50
 
-#define ALWAYS_SCAN false
 unsigned long last_range_update;
 #define RANGE_UPDATE_MS 100
-unsigned long last_move_time;
-#define MOVE_STOP_DELAY_MS 500
 
 void setup() {
 
@@ -106,7 +103,7 @@ void setup() {
         request->send(400, "application/json", "{\"error\":\"missing rpm\"}");
        return;
       }
-      float rpm = request->getParam("set_rpm")->value().toFloat();
+      int rpm = request->getParam("set_rpm")->value().toInt();
       cmd.type = Command::SET_RPM;
       cmd.params.set_rpm.rpm = rpm;
     } else if(type == "status"){
@@ -146,10 +143,9 @@ void setup() {
   
   Serial.println("entering loop: ");
 
-  unsigned long now = millis();
-  last_position_update = now;
-  last_range_update = now; 
-  last_move_time = now; 
+  last_position_update = millis();
+  last_range_update = millis();
+  
 }
 
 
@@ -159,19 +155,11 @@ void loop() {
     last_position_update = now;
     my_roombot.update_position();
   }
- 
-  if(my_roombot.is_moving()){
-    last_move_time = now;
-  }
+  
   if(now - last_range_update > RANGE_UPDATE_MS){
-    if((now - last_move_time < MOVE_STOP_DELAY_MS) || (ALWAYS_SCAN == true)){
-      last_range_update = now;
-      StatusData status = my_roombot.get_status();
-      events.send(String(status.range).c_str(), "range");
-      String telemetry = "{\"x\":" + String(status.x) + ",\"y\":" + String(status.y) + ",\"angle\":" + 
-                          String(status.angle) + ",\"range\":" + String(status.range) + "}";
-      events.send(String(telemetry).c_str(), "telemetry");
-    }
+    last_range_update = now;
+    int distance = my_roombot.scan_once();
+    events.send(String(distance).c_str(), "range");
   }
 }
 
