986eb82df8adff2becf14f2a623c8e7523033298
NicholasLeeEvans <evansnicholaslee@gmail.com>
2026-05-04 22:51:52 +0900

    fix: IR ISR call - remove analog read from ISR call - remove single/multiple read functions from RangeFinder - change ISR call to flag every 40ms for one call instead of 300ms for 7 calls

--- lib/RangeFinder/RangeFinder.cpp
+++ lib/RangeFinder/RangeFinder.cpp
@@ -1,6 +1,7 @@
 #include "RangeFinder.h"
 
-#define ISR_TIMER_DIVIDER   80
+#define ISR_TIMER_DIVIDER       80
+#define SCAN_TIMER_INTERVAL_US  (40 * 1000)
 
 #define STORED_READS    7
 #define MEDIAN_ITR      3
@@ -16,8 +17,7 @@ RangeFinder::RangeFinder(int pin_number){
 void RangeFinder::set_timer(int timer_number, void (*isr)()){
     Timer_cfg = timerBegin(timer_number, ISR_TIMER_DIVIDER, true); 
     timerAttachInterrupt(Timer_cfg, isr, true);
-    //timerAlarmWrite(Timer_cfg, 500, true);
-    timerAlarmWrite(Timer_cfg, 300 * 1000, true); //for variable interrupt timing
+    timerAlarmWrite(Timer_cfg, SCAN_TIMER_INTERVAL_US, true);
     timerAlarmEnable(Timer_cfg);
 }
 
@@ -27,33 +27,18 @@ void RangeFinder::clear_stored_vals(){
     }
 }
 
-void RangeFinder::take_single_reading(){
+void RangeFinder::sample(){
     analogRead(this->pin_number);
     this->stored_vals[iterator] = analogRead(this->pin_number);
     iterator = (iterator + 1) % STORED_READS;
 }
 
-void RangeFinder::take_multiple_readings(int count){
-    if(count < 1){
-        count = 1;
-    }else if(count > STORED_READS){
-        count = STORED_READS;
-    }
-        
-    for(int itr = 0; itr < count; itr++){
-        this->take_single_reading();
-    }
-
-}
-
 int RangeFinder::get_range_value(){
 
     int read_values[STORED_READS];
-    noInterrupts();
     for(int i = 0; i < STORED_READS; i++) {
         read_values[i] = stored_vals[i];
     }
-    interrupts();
     for(int itr = 1; itr < STORED_READS; itr++)
     {
         for(int itr2 = itr; itr2 > 0; itr2--){
